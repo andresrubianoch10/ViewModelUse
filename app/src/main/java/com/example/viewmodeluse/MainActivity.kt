58098d444f1356f743c0d9ae80c4e8aac98b3a4b
andres.rubiano <andres.rubiano.ch@gmail.com>
2022-05-31 18:31:05 -0500

    Added code without view model

--- app/src/main/java/com/example/viewmodeluse/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeluse/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.viewmodeluse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewmodeluse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with (binding.mainText) {
+            text = "1"
+            setOnClickListener {
+                var actualValue = 0
+                actualValue += (text as String).toInt() + 1
+                text = actualValue.toString()
+            }
+        }
+
     }
 }
