37dcf7c1f70b2425ed26d3bc0903f2c29961663a
andres.rubiano <andres.rubiano.ch@gmail.com>
2022-05-31 18:48:18 -0500

    Applying view model in activity

--- app/src/main/java/com/example/viewmodeluse/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeluse/MainActivity.kt
@@ -3,22 +3,26 @@ package com.example.viewmodeluse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodeluse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
         with (binding.mainText) {
-            text = "1"
+            text = viewModel.getCurrentCount().toString()
             setOnClickListener {
-                var actualValue = 0
-                actualValue += (text as String).toInt() + 1
-                text = actualValue.toString()
+                text = viewModel.getUpdatedCount().toString()
             }
         }
     }
